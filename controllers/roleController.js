4e27f36b15f17d9aa5378b958ed5f7b837935f05
Selena <selena.k.rodriguez@gmail.com>
2023-10-11 14:32:45 -0500

    prettier.

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -73,70 +73,70 @@ exports.role_mages = asyncHandler(async (req, res, next) => {
 });
 
 exports.role_marksmen = asyncHandler(async (req, res, next) => {
-    try {
-        // Find the role with the name "Marksmen"
-        const marksmenRole = await Roles.findOne({ role: "Marksmen" });
-        if (!marksmenRole) {
-          // If no "Marksmen" found, redirect to catalog
-          return res.redirect("/catalog");
-        }
-    
-        // Find the champions with the "Marksmen" role.
-        const marksmenChampions = await Champions.find({ role: marksmenRole._id })
-          .populate("role")
-          .exec();
-    
-        res.render("role_marksmen", {
-          title: "League Marksmen",
-          role_marksmen: marksmenChampions,
-        });
-      } catch (error) {
-        next(error);
-      }
+  try {
+    // Find the role with the name "Marksmen"
+    const marksmenRole = await Roles.findOne({ role: "Marksmen" });
+    if (!marksmenRole) {
+      // If no "Marksmen" found, redirect to catalog
+      return res.redirect("/catalog");
+    }
+
+    // Find the champions with the "Marksmen" role.
+    const marksmenChampions = await Champions.find({ role: marksmenRole._id })
+      .populate("role")
+      .exec();
+
+    res.render("role_marksmen", {
+      title: "League Marksmen",
+      role_marksmen: marksmenChampions,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 exports.role_supports = asyncHandler(async (req, res, next) => {
-    try {
-        // Find the role with the name "Supports"
-        const supportsRole = await Roles.findOne({ role: "Supports" });
-        if (!supportsRole) {
-          // If no "Supports" found, redirect to catalog
-          return res.redirect("/catalog");
-        }
-    
-        // Find the champions with the "Supports" role.
-        const supportChampions = await Champions.find({ role: supportsRole._id })
-          .populate("role")
-          .exec();
-    
-        res.render("role_supports", {
-          title: "League Supports",
-          role_supports: supportChampions,
-        });
-      } catch (error) {
-        next(error);
-      }
+  try {
+    // Find the role with the name "Supports"
+    const supportsRole = await Roles.findOne({ role: "Supports" });
+    if (!supportsRole) {
+      // If no "Supports" found, redirect to catalog
+      return res.redirect("/catalog");
+    }
+
+    // Find the champions with the "Supports" role.
+    const supportChampions = await Champions.find({ role: supportsRole._id })
+      .populate("role")
+      .exec();
+
+    res.render("role_supports", {
+      title: "League Supports",
+      role_supports: supportChampions,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 exports.role_tanks = asyncHandler(async (req, res, next) => {
-    try {
-        // Find the role with the name "Tanks"
-        const tanksRole = await Roles.findOne({ role: "Tanks" });
-        if (!tanksRole) {
-          // If no "Tanks" found, redirect to catalog
-          return res.redirect("/catalog");
-        }
-    
-        // Find the champions with the "Supports" role.
-        const tankChampions = await Champions.find({ role: tanksRole._id })
-          .populate("role")
-          .exec();
-    
-        res.render("role_tanks", {
-          title: "League Supports",
-          role_tanks: supportChampions,
-        });
-      } catch (error) {
-        next(error);
-      }
+  try {
+    // Find the role with the name "Tanks"
+    const tanksRole = await Roles.findOne({ role: "Tanks" });
+    if (!tanksRole) {
+      // If no "Tanks" found, redirect to catalog
+      return res.redirect("/catalog");
+    }
+
+    // Find the champions with the "Supports" role.
+    const tankChampions = await Champions.find({ role: tanksRole._id })
+      .populate("role")
+      .exec();
+
+    res.render("role_tanks", {
+      title: "League Supports",
+      role_tanks: tankChampions,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
